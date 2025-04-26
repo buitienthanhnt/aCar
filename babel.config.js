cad9f967f5c2e80de86e8e43441161da2d5d1bfe
thanhnt <buitienthanhnt@gmail.com>
2025-04-26 19:31:07 +0700

    dev: Up code for app icon, tailwind css, alias path module

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          '@': './src',
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@scenes': './src/scenes',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
+};
---
+++ src/theme/Base.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Base = ()=>{
+    return <View className={'bg-white rounded p-1'}>
+        <Text className={'font-bold text-xl text-blue-500'}>Hello 222345</Text>
+        <Text className={'font-bold text-xl text-green-700'}>It`s a-car</Text>
+    </View>;
+};
+
+export default Base;

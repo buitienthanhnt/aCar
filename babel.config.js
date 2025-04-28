00a4621a198dcfe532571d6e7a835715c26b4241
thanhnt <buitienthanhnt@gmail.com>
2025-04-28 22:31:10 +0700

    dev: Up code for helper function

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
   plugins: [
-   [
-     'module-resolver',
-     {
-       "root": ["./src"],
-       alias: {
-         '@': './src',
-         '@api': './src/api',
-         '@assets': './src/assets',
-         '@components': './src/components',
-         '@utils': './src/utils',
-       },
-     },
-   ],
-  'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          '@': './src',
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@scenes': './src/scenes',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+          '@type': './src/type',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
   ],
 };

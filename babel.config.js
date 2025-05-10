adb31052519d93a0b6d496afee9f3757ac71db81
thanhnt <buitienthanhnt@gmail.com>
2025-05-10 22:59:24 +0700

    dev-alias-path: Update code

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,27 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
   plugins: [
-   [
-     'module-resolver',
-     {
-       "root": ["./src"],
-       alias: {
-         '@': './src',
-         '@api': './src/api',
-         '@assets': './src/assets',
-         '@components': './src/components',
-         '@utils': './src/utils',
-       },
-     },
-   ],
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@scenes': './src/scenes',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+          '@elements': './src/elements',
+          '@screens': './src/screens',
+          '@type': './src/type',
+          '@data': './src/data',
+          '@styles': './src/styles',
+        },
+      },
+    ],
       'react-native-reanimated/plugin',
   ],
 };

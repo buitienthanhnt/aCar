a2489a0e1f7f02b49433aa9fe0cda45aa089ad8d
thanhnt <buitienthanhnt@gmail.com>
2025-07-08 21:40:16 +0700

    dev: Init for react-native new version 0.8 and support sdk 35

---
+++ App.tsx
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { useEffect } from 'react';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(()=>{
+    console.log(123);
+    
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      {/* <NewAppScreen templateFileName="App.tsx" /> */}
+      <Text style={{
+        color: 'blue',
+        fontSize: 20,
+        fontWeight: '700'
+      }}>123 zxc</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    paddingLeft: 8,
+    justifyContent: 'center'
+  },
+});
+
+export default App;

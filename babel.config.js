155b94cd5ceaf814f74037282dcc6d507bb57419
thanhnt <buitienthanhnt@gmail.com>
2025-07-09 00:02:58 +0700

    dev: Up code for setup alias path module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+   [
+     'module-resolver',
+     {
+       "root": ["./src"],
+       alias: {
+         '@': './src',
+         '@api': './src/api',
+         '@assets': './src/assets',
+         '@components': './src/components',
+         '@utils': './src/utils',
+       },
+     },
+   ],
+  ],
 };
